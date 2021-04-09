0e470a3600149a6915ae5fe6d24d61400e742b8a
Mary C <mcn4c@virginia.edu>
2021-04-09 11:52:09 -0400

    added assets folder containing css and js folders to restructure public folder

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+// require path?
+const db = require('./models');
 
 const PORT = process.env.PORT || 3000;
 
-
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use(require("./public/api"));
+//is this correct?
+app.use(require('./public/api'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
