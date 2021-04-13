b0cf4b764f8d45a5519d74b88393aa3066b59615
Mary C <mcn4c@virginia.edu>
2021-04-13 11:35:56 -0400

    imported routes into server.js file and updated connection to mongoose db

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-// require path?
 const db = require('./models');
 
 const PORT = process.env.PORT || 3000;
@@ -15,10 +14,19 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//is this correct?
+//not sure about this
 app.use(require('./public/assets/js/api'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+});
+
+//import routes
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
