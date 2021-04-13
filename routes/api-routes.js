5a8e75738810a06692acc9f1438ba3178b69aa4e
Mary C <mcn4c@virginia.edu>
2021-04-13 11:19:48 -0400

    added route to get exercises

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -36,4 +36,16 @@ module.exports = (app) => {
 			}
 		);
 	});
+
+	// get exercises
+	app.get('/api/workouts', (req, res) => {
+		db.Exercise
+			.find({})
+			.then((dbExercise) => {
+				res.json(dbExercise);
+			})
+			.catch((err) => {
+				res.status(400).json(err);
+			});
+	});
 };
